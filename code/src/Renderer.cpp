9113d65b763f7f619f8e786801af921a8dfa1276
xj-wu22 <xj-wu22@mails.tsinghua.edu.cn>
2024-07-03 02:34:09 +0800

    多线程

--- code/src/Renderer.cpp
+++ code/src/Renderer.cpp
@@ -1,11 +1,13 @@
 #include <fstream>
 #include "Scene.hpp"
 #include "Renderer.hpp"
-
+#include <mutex>
+#include <thread>
 
 inline float deg2rad(const float& deg) { return deg * M_PI / 180.0; }
 
 const float EPSILON = 0.00001;
+std::mutex mtx;
 
 // The main render function. This where we iterate over all pixels in the image,
 // generate primary rays and cast these rays into the scene. The content of the
@@ -17,35 +19,83 @@ void Renderer::Render(const Scene& scene)
     float scale = tan(deg2rad(scene.fov * 0.5));
     float imageAspectRatio = scene.width / (float)scene.height;
     Vector3f eye_pos(278, 273, -800);
-    int m = 0;
-
     // change the spp value to change sample ammount
     int spp = 32;                 // spp指每个pixel会采样的次数
     std::cout << "SPP: " << spp << "\n";
-    for (uint32_t j = 0; j < scene.height; ++j) {
-        for (uint32_t i = 0; i < scene.width; ++i) {
-            for (int k = 0; k < spp; k++){
-                // 计算 step 的值
-                float step = 1.0 / sqrt(spp);
+    //int m = 0;
+    // for (uint32_t j = 0; j < scene.height; ++j) {
+        // for (uint32_t i = 0; i < scene.width; ++i) {
+        //     for (int k = 0; k < spp; k++) {
+        //         // 超采样抗锯齿（SSAA） 随机超采样（Random Sampling SSAA）
+        //         // 生成随机偏差 [-0.5, 0.5]
+        //         float offset_x = ((rand() / (float)RAND_MAX) - 0.5f);
+        //         float offset_y = ((rand() / (float)RAND_MAX) - 0.5f);
 
-                // 计算子像素中心位置
-                float x = (2 * (i + step / 2 + step * (k % (int)sqrt(spp))) / (float)scene.width - 1) * imageAspectRatio * scale;
-                float y = (1 - 2 * (j + step / 2 + step * (k / (int)sqrt(spp))) / (float)scene.height) * scale;
+        //         // 计算子像素位置
+        //         float x = (2 * (i + 0.5f + offset_x) / (float)scene.width - 1) * imageAspectRatio * scale;
+        //         float y = (1 - 2 * (j + 0.5f + offset_y) / (float)scene.height) * scale;
 
-                // 计算光线方向
-                Vector3f dir = Vector3f(-x, y, 1).normalized();
+        //         // 计算光线方向
+        //         Vector3f dir = Vector3f(-x, y, 1).normalized();
 
-                // 发射光线并累加颜色值
-                framebuffer[m] += scene.castRay(Ray(eye_pos, dir), 0) / spp;  
+        //         // 发射光线并累加颜色值
+        //         framebuffer[m] += scene.castRay(Ray(eye_pos, dir), 0) / spp;
+        //     }
+        //     m++;
+    //     }
+    //     UpdateProgress(j / (float)scene.height);
+    // }
+    // UpdateProgress(1.f);
+
+    int process = 0;
+    const int thred = 20;
+    int times = scene.height / thred;  // 每个线程的行数
+    std::thread th[thred];
+
+    auto castRayMultiThread = [&](uint32_t y_min, uint32_t y_max)
+    {
+        for(uint32_t j = y_min; j < y_max; j++)
+        {
+            
+            int m = j * scene.width;
+            for (uint32_t i = 0; i < scene.width; ++i) {
+                for (int k = 0; k < spp; k++) {
+                    // 超采样抗锯齿（SSAA） 随机超采样（Random Sampling SSAA）
+                    // 生成随机偏差 [-0.5, 0.5]
+                    float offset_x = ((rand() / (float)RAND_MAX) - 0.5f);
+                    float offset_y = ((rand() / (float)RAND_MAX) - 0.5f);
+
+                    // 计算子像素位置
+                    float x = (2 * (i + 0.5f + offset_x) / (float)scene.width - 1) * imageAspectRatio * scale;
+                    float y = (1 - 2 * (j + 0.5f + offset_y) / (float)scene.height) * scale;
+
+                    // 计算光线方向
+                    Vector3f dir = Vector3f(-x, y, 1).normalized();
+
+                    // 发射光线并累加颜色值
+                    framebuffer[m] += scene.castRay(Ray(eye_pos, dir), 0) / spp;
+                }
+                m++;
             }
-            m++;
+            mtx.lock();
+            process++;
+            UpdateProgress(1.0 * process / scene.height);
+            mtx.unlock();
         }
-        UpdateProgress(j / (float)scene.height);
+    };
+
+
+    //分行进行路径追踪
+    for (int i = 0; i < thred; i++) {//从第0行出发，一共有0~by-1行
+        th[i] = std::thread(castRayMultiThread, i * times, (i + 1) * times);
+    }
+    //每个线程执行join
+    for (int i = 0; i < thred; i++) {
+        th[i].join();
     }
     UpdateProgress(1.f);
 
     // save framebuffer to file
-
     FILE* fp = fopen("binary.ppm", "wb");
     (void)fprintf(fp, "P6\n%d %d\n255\n", scene.width, scene.height);
     for (auto i = 0; i < scene.height * scene.width; ++i) {
